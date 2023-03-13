b6722bbd6603cbd7f34e849de612aa70950f698c
João Zenaro Laptop Samsung <zenaro@inovea.io>
2023-03-13 14:09:02 -0300

    ch 5 ex 1

---
+++ Chapter5/Exercises/Exerc5_01/Program.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chapter5.Exercises.Exerc5_01
+{
+    class Program
+    {
+        private static long Fibonacci(int n)
+        {
+            if (n <= 2L)
+                return 1L;
+
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+        public static void Main()
+        {
+            string input;
+            do
+            {
+                Console.Write("Enter number: ");
+                input = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(input) && int.TryParse(input, NumberStyles.Any, CultureInfo.CurrentCulture, out var number))
+                {
+                    Task.Run(() =>
+                    {
+                        var now = DateTime.Now;
+                        var fib = Fibonacci(number);
+                        var duration = DateTime.Now.Subtract(now);
+                        Logger.Log($"Fibonacci {number:N0} = {fib:N0} (elapsed time: {duration.TotalSeconds:N0} secs)");
+                    });
+                }
+
+            } while (input != string.Empty);
+        }
+    }
+}
