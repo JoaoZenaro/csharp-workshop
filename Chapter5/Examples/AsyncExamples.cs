25c407ca27a794b1b7e63374cacae1953aeb7d75
João Zenaro Laptop Samsung <zenaro@inovea.io>
2023-03-13 16:44:46 -0300

    example async

---
+++ Chapter5/Examples/AsyncExamples.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chapter5.Examples
+{
+    public class AsyncExamples
+    {
+        public static async Task Main()
+        {
+            Logger.Log("Starting");
+            await BuildGreetings();
+
+            Logger.Log("Press Enter");
+            Console.ReadLine();
+        }
+
+        private static async Task BuildGreetings()
+        {
+            var message = "Morning";
+            Logger.Log($"{message}");
+
+            await Task.Delay(TimeSpan.FromSeconds(10D));
+
+            message += "...Afternoon";
+            Logger.Log($"{message}");
+
+            await Task.Delay(TimeSpan.FromSeconds(1D));
+
+            message += "...Evening";
+            Logger.Log($"{message}");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # C# Workshop
 
-Page: 415
+Page: 425
